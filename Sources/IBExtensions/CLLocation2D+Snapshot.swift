e008eaa512d026b7ae6d5fc6813e1c5587fb47ff
Ingo Boehme <ingo@puco.de>
2020-06-27 16:15:13 +0200

    Before Trying to fix watch reload problem

--- Sources/IBExtensions/CLLocation2D+Snapshot.swift
+++ Sources/IBExtensions/CLLocation2D+Snapshot.swift
@@ -29,23 +29,19 @@ public extension FileManager {
 #if os(iOS)
 
 public extension CLLocationCoordinate2D {
-    
     static func clearCachedImages() {
         let fileManager = FileManager.default
         try? fileManager.removeItem(at: FileManager.fileURL(name: "maps", inCache: true))
-
     }
-    
-    
+
     func mkMapImage(
-        radius: Double = 500,
         size: CGSize = CGSize(width: 600, height: 400),
         mapType: MKMapType = .satelliteFlyover,
         closure: @escaping (UIImage) -> ()) {
         let darkMode: Bool = UIScreen.main.traitCollection.userInterfaceStyle == .dark
 
         try? FileManager.default.createDirectory(at: FileManager.fileURL(name: "maps", inCache: true), withIntermediateDirectories: true, attributes: nil)
-        let imageName = "\(self.latitude.latitudeString) \(self.longitude.longitudeString)  r\(Int(radius)) s\(Int(size.width)) \(Int(size.height))  \(darkMode ? "d" : "l")"
+        let imageName = "\(self.latitude.latitudeString) \(self.longitude.longitudeString)  s\(Int(size.width)) \(Int(size.height))  \(darkMode ? "d" : "l")"
         let fileUrl = FileManager.fileURL(name: "maps/\(imageName)", inCache: true)
 
         if FileManager.default.fileExists(atPath: fileUrl.path),
@@ -53,7 +49,7 @@ public extension CLLocationCoordinate2D {
             closure(image)
             return
         } else {
-            self.screenshot(radius: radius, size: size, mapType: mapType) { image in
+            self.screenshot(size: size, mapType: mapType) { image in
                 if let image = image {
                     if let data = image.jpegData(compressionQuality: 0.9) {
                         try? data.write(to: fileUrl)
@@ -65,35 +61,80 @@ public extension CLLocationCoordinate2D {
         }
     }
 
-    func screenshot(radius meter: Double, size: CGSize = CGSize(width: 300, height: 300), mapType: MKMapType = .satellite, closure: @escaping (UIImage?) -> ()) {
+    func screenshot(altitude: Double = 100, pitch: CGFloat = 45, size: CGSize = CGSize(width: 300, height: 300), mapType: MKMapType = .satelliteFlyover, closure: @escaping (UIImage?) -> ()) {
         let mapSnapshotOptions = MKMapSnapshotter.Options()
 
         // Set the region of the map that is rendered.
-        let location = self // Apple HQ
-        let region = MKCoordinateRegion(center: location, latitudinalMeters: meter, longitudinalMeters: meter)
-        mapSnapshotOptions.region = region
+//        let location = self // Apple HQ
+//        let region = MKCoordinateRegion(center: location, latitudinalMeters: 0 , longitudinalMeters: 0)
+//        mapSnapshotOptions.region = region
 
         // Set the scale of the image. We'll just use the scale of the current device, which is 2x scale on Retina screens.
         mapSnapshotOptions.scale = UIScreen.main.scale
-        
-//        let camera = MKMapCamera(lookingAtCenter: self, fromDistance: 1000, pitch: 400, heading: 0)
-//        mapSnapshotOptions.camera = camera
+
+//        let userCoordinate = self
+//        let eyeCoordinate = self.shift(by: CGPoint(x: 0, y: 0.005))
+        // let mapCamera = MKMapCamera(lookingAtCenter: userCoordinate, fromEyeCoordinate: eyeCoordinate, eyeAltitude: 500) // altitude)
+//        let mapCamera = MKMapCamera(lookingAtCenter: self, fromDistance: altitude, pitch: pitch, heading: 0)
+        let mapCamera = MKMapCamera(lookingAtCenter: self, fromDistance: 1000, pitch: 15, heading: 0)
+
+        mapSnapshotOptions.camera = mapCamera
 
         mapSnapshotOptions.traitCollection = UIScreen.main.traitCollection
-
 
         // Set the size of the image output.
         mapSnapshotOptions.size = size
 
         // Show buildings and Points of Interest on the snapshot
-        mapSnapshotOptions.showsBuildings = true
         mapSnapshotOptions.pointOfInterestFilter = MKPointOfInterestFilter(excluding: [])
         mapSnapshotOptions.mapType = mapType
-//        mapSnapshotOptions.showsPointsOfInterest = true
 
         let snapShotter = MKMapSnapshotter(options: mapSnapshotOptions)
+
         snapShotter.start(with: .main, completionHandler: { snapshot, _ in
-            closure(snapshot?.image)
+
+            if let snapshot = snapshot {
+                let image = UIGraphicsImageRenderer(size: mapSnapshotOptions.size).image { _ in
+                    snapshot.image.draw(at: .zero)
+
+                    if let pinImage = UIImage(systemName: "arrow.down.left")?.withTintColor(.white) { // UIImage(named: "positionMarker") {
+                        var point = snapshot.point(for: self)
+
+                        if CGRect(origin: .zero, size: mapSnapshotOptions.size).contains(point) {
+//                            point.x -= pinImage.size.width / 2
+//                            point.y -= pinImage.size.height / 2
+//                            point.x -= pinImage.size.width / 2
+                            point.y -= pinImage.size.height 
+                            pinImage.draw(at: point)
+                        }
+                    }
+                }
+
+                closure(image)
+
+//                var dict: [String: Int] = [:]
+//                for _ in 0...100 {
+//                    if let hex = image.randomPixelColor().toHex() {
+//                        if let count = dict[hex] {
+//                            dict[hex] = count + 1
+//                        } else {
+//                            dict[hex] = 1
+//                        }
+//                    }
+//                }
+//                for (_, value) in dict {
+//                    if value > 80 {
+//                        if altitude < 1000 {
+//                            self.screenshot(altitude: altitude + 100, pitch: pitch / 3 * 2, size: size, mapType: mapType, closure: closure)
+//                        } else {
+//                            closure(nil)
+//                        }
+//                        return
+//                    }
+//                }
+//                print("***",location.latitude.latitudeString, location.longitude.longitudeString, altitude, pitch)
+                //               closure(image)
+            }
         })
     }
 }
